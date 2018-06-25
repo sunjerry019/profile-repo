2ddb305f4c70c94b8495a953ec9e4eff888d3e38
Sun Yudong <sunjerry019@gmail.com>
2018-06-25 23:31:53 +0800

    added main body stuff

--- strings.php
+++ strings.php
@@ -31,7 +31,18 @@
         "small_expertise"   => "Language Enthusiast, Photographer",
         "ptg"               => "The collection of pixels I have taken",
         "about_me"          => "About Me",
-        "github"            => "My GitHub Page"
+        "github"            => "My GitHub Page",
+
+        "personal_info"     => "Personal Info",
+        "name_label"        => "Name",
+        "name_2"            => "SUN Yudong",
+        "dob_label"         => "Born",
+        "dob"               => "29 July 1998",
+        "dob_place"         => "Shanghai, China",
+        "nationality_label" => "Nationality",
+        "nationality"       => "Singapore",
+        "email_label"       => "E-Mail Address",
+        "email"             => "<span class='mono'>yudongsun019 [at] gmail [dot] com</span>"
     );
 
     $de = array(
@@ -40,7 +51,18 @@
         "small_expertise"   => "Sprachenthusiast, Photograf",
         "ptg"               => "Eine Sammlung aus meiner Kamera",
         "about_me"          => "Über Mich",
-        "github"            => "Meine GitHub Seite"
+        "github"            => "Meine GitHub Seite",
+
+        "personal_info"     => "Persönliche Daten",
+        "name_label"        => "Name",
+        "name_2"            => "SUN Yudong",
+        "dob_label"         => "Geboren",
+        "dob"               => "29. Juli 1998",
+        "dob_place"         => "Shanghai, China",
+        "nationality_label" => "Nationalität",
+        "nationality"       => "Singapur",
+        "email_label"       => "E-Mail-Adresse",
+        "email"             => "<span class='mono'>yudongsun019 [at] gmail [dot] com</span>"
     );
 
     $zh = array(
@@ -49,7 +71,18 @@
         "small_expertise"   => "语言、摄影爱好者",
         "ptg"               => "我拍的一些照片",
         "about_me"          => "我的履历",
-        "github"            => "我的GitHub页面"
+        "github"            => "我的GitHub页面",
+
+        "personal_info"     => "个人信息",
+        "name_label"        => "姓名",
+        "name_2"            => "孙毓栋",
+        "dob_label"         => "出生",
+        "dob"               => "<span class='mono'>29/07/1998</span>",
+        "dob_place"         => "中国，上海",
+        "nationality_label" => "国际",
+        "nationality"       => "新加坡",
+        "email_label"       => "电邮",
+        "email"             => "<span class='mono'>yudongsun019 [at] gmail [dot] com</span>"
     );
 
     $strings = array(
