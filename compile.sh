ecda23ec195d329576d4df146ca1656616f383f2
Sun Yudong <sunjerry019@gmail.com>
2018-09-20 20:40:34 +0800

    add compile script

---
+++ compile.sh
@@ -0,0 +1,16 @@
+FILE="cv"
+
+svgtopdf "$FILE.svg"
+pdftk "$FILE.pdf" dump_data output dump.txt
+
+head -n 1 dump.txt > dump.2.txt
+cat metadata.txt | tee -a dump.2.txt 
+tail -n +2 dump.txt | tee -a dump.2.txt
+
+pdftk "$FILE.pdf" update_info dump.2.txt output "$FILE.copy.pdf"
+mv "$FILE.copy.pdf" "$FILE.pdf"
+rm dump.txt dump.2.txt
+
+
+# exiftool -Title="Sun Yudong - Curriculum Vitae" -Author="Sun Yudong" -overwrite_original resume.pdf
+
