371a6284a637e8bb53780d8a0c550802989559d7
Sun Yudong <sunjerry019@gmail.com>
2021-09-13 06:47:02 +0200

    replace strings code with code that gets from a csv

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <html <?php if($lang == "zh"){ echo "class='hl_zh'"; } ?> >
     <head>
-        <title>Yudong Sun</title>
+        <title><?php echo $strings["name"][$lang] ?></title>
         <link href="//fonts.googleapis.com/css?family=Roboto+Mono|Source+Sans+Pro" rel="stylesheet">
         <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         <!-- <link rel="stylesheet" href="assets/flag-icon-css-master/css/flag-icon.min.css" type="text/css"/> -->
@@ -29,30 +29,30 @@
                     <div class="photoflank"></div>
                 </div>
                 <div id="name_expertise">
-                    <span id="name"><?php echo $strings[$lang]["name"] ?></span>
-                    <span id="expertise"><?php echo $strings[$lang]["expertise"] ?></span>
-                    <span id="small_expertise"><?php echo $strings[$lang]["small_expertise"] ?></span>
+                    <span id="name"><?php echo $strings["name"][$lang] ?></span>
+                    <span id="expertise"><?php echo $strings["expertise"][$lang] ?></span>
+                    <span id="small_expertise"><?php echo $strings["small_expertise"][$lang] ?></span>
                     <span id="countries">
-                         // <?php echo $strings[$lang]["countries"] ?> //
+                         // <?php echo $strings["countries"][$lang] ?> //
                     </span>
                 </div>
                 <div id="languages" class="force-en-font">
                     <a href="?hl=en">EN</a><a href="?hl=de">DE</a><a href="?hl=zh">ZH</a>
                 </div>
                 <div id="linkbuttons">
-                    <a href="//www.linkedin.com/in/sunjerry019/" title="<?php echo $strings[$lang]['linkedin'] ?>"><i class="fab fa-linkedin-in"></i></a>
-                    <a href="?expand" id="linkbtn_expand" title="<?php echo $strings[$lang]['about_me'] ?>">
-                        <span><?php echo $strings[$lang]["about_me"] ?></span>
+                    <a href="//www.linkedin.com/in/sunjerry019/" title="<?php echo $strings['linkedin'][$lang] ?>"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="?expand" id="linkbtn_expand" title="<?php echo $strings['about_me'][$lang] ?>">
+                        <span><?php echo $strings["about_me"][$lang] ?></span>
                         <i class="fas fa-chevron-down"></i>
                     </a>
-                    <a href="//github.com/sunjerry019" title="<?php echo $strings[$lang]['github'] ?>"><i class="fab fa-github-alt"></i></a>
+                    <a href="//github.com/sunjerry019" title="<?php echo $strings['github'][$lang] ?>"><i class="fab fa-github-alt"></i></a>
                 </div>
             </div>
         </div>
 
         <div id="body">
             <div id="projects" class="bulk">
-                <h2><?php echo $strings[$lang]["projects"] ?></h2>
+                <h2><?php echo $strings["projects"][$lang] ?></h2>
                 <div class="content">
                     <div class="project">
                 </div>
